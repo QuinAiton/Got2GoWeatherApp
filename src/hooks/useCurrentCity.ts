52aefde19d4ad52aff633ed548114f73c7d5e442
Quinten <quinaiton@hotmail.com>
2023-08-01 12:52:52 +1000

    create hook to retreive user location and reverse geocodes for city name

---
+++ src/hooks/useCurrentCity.ts
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+type CurrentCityData = {
+  city: string | null;
+  error: Error | null;
+};
+
+export default function useCurrentCity(): CurrentCityData {
+  const [city, setCity] = useState<string | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const getCurrentCity = async (): Promise<void> => {
+      try {
+        if (!navigator.geolocation) {
+          throw new Error("Geolocation is not supported by your browser");
+        }
+
+        navigator.geolocation.getCurrentPosition(
+          async (position) => {
+            const { latitude, longitude } = position.coords;
+
+            const response = await fetch(
+              `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${
+                import.meta.env.VITE_GEOCODE_API_KEY
+              }`
+            );
+            const data = await response.json();
+
+            if (response.ok && data.results.length > 0) {
+              const city = data.results[0].components.city;
+              setCity(city);
+            } else {
+              throw new Error("City not found");
+            }
+          },
+          (error) => {
+            throw error;
+          }
+        );
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    getCurrentCity();
+  }, []);
+
+  return { city, error };
+}
