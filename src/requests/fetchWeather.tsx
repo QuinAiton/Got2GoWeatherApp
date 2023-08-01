0817039bcccb9654b5e1b577230f19c7ca9d6186
Quinten <quinaiton@hotmail.com>
2023-08-01 11:25:20 +1000

    updates api endpoint adds try catch block

--- src/requests/fetchWeather.tsx
+++ src/requests/fetchWeather.tsx
@@ -1,7 +1,21 @@
+import cleanWeatherData from "../utils/cleanWeatherData";
+
 export default async function fetchWeather(location: string) {
-  const url = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${
-    import.meta.env.VITE_OPEN_WEATHER_KEY
-  }`;
-  const response = await fetch(url);
-  return response.json();
+  try {
+    const url = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${location}&limit=5&appid=${
+      import.meta.env.VITE_OPEN_WEATHER_KEY
+    }`;
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch weather data");
+    }
+
+    const weatherData = await response.json();
+    return cleanWeatherData(weatherData);
+  } catch (error) {
+    // Handle the error
+    console.error(error);
+    throw error; // Rethrow the error or handle it accordingly
+  }
 }
