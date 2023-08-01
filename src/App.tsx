e1d477c3fd59306ecac8b208d7f40e3dc96cfbef
Quinten <quinaiton@hotmail.com>
2023-08-01 10:08:11 +1000

    Sets up initial styles and background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import LocationDropDown from "components/LocationDropDown";
 import React from "react";
 import fetchWeather from "./requests/fetchWeather";
 import { useQuery } from "@tanstack/react-query";
+
 export default function App() {
   const { data, error } = useQuery(["weather", "london"], () =>
     fetchWeather("london")
   );
 
-  return <div className="text-black">App start</div>;
+  return (
+    <div className="bg-[url('/public/assets/background.jpg')] bg-center w-screen h-screen flex justify-center items-center p-20">
+      <LocationDropDown />
+    </div>
+  );
 }
