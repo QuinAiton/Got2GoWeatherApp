5427412ecbdcca9fae6a64a29dc420901f4d7369
Quinten <quinaiton@hotmail.com>
2023-08-01 11:25:42 +1000

    moves types to their own files

---
+++ src/types/rawWeatherData.ts
@@ -0,0 +1,18 @@
+export type rawWeatherData = {
+  weather: [
+    {
+      description: string;
+      icon: string;
+    }
+  ];
+  main: {
+    temp: number;
+    feels_like: number;
+    humidity: number;
+  };
+  wind: {
+    speed: number;
+  };
+  timezone: string;
+  dt: string;
+};
---
+++ src/types/weatherData.ts
@@ -0,0 +1,10 @@
+export type weatherData = {
+  weather: string;
+  description: string;
+  icon: string;
+  feels_like: number;
+  humidity: number;
+  temp: number;
+  wind_speed: number;
+  date: string;
+};
