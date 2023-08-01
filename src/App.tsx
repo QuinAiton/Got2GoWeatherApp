3d4d58e869b778e8b90c458b5fc8102baa55c7f1
Quinten <quinaiton@hotmail.com>
2023-08-01 12:00:05 +1000

    Adds spinner component if data is fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import LocationDropDown from "components/LocationDropDown";
 import React from "react";
+import Spinner from "components/Spinner";
+import WeatherTile from "components/WeatherTile";
 import fetchWeather from "./requests/fetchWeather";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 export default function App() {
-    const [selectLocation, setSelectedLocation] = useState("Miami");
+  const [selectLocation, setSelectedLocation] = useState("Miami");
 
-  const { data, error } = useQuery(["weather", "london"], () =>
-    fetchWeather("london")
+  const { isLoading, error, data } = useQuery(["weather", selectLocation], () =>
+    fetchWeather(selectLocation)
   );
+
+  if (error) return <div>Something went wrong</div>;
 
   return (
     <div className="bg-[url('/public/assets/background.jpg')] bg-center w-screen h-screen flex justify-center items-center p-20">
-      <LocationDropDown setSelectedLocation={setSelectedLocation} selectLocation={selectLocation} />
+      {isLoading && <Spinner />}
+      {data && (
+        <div className="flex flex-col gap-20 ">
+          <LocationDropDown
+            setSelectedLocation={setSelectedLocation}
+            selectLocation={selectLocation}
+          />
+          <WeatherTile weatherData={data} selectedLocation={selectLocation} />
+        </div>
+      )}
     </div>
   );
 }
